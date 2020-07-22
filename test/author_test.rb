2f7a8059a07b3b5caf88a866d7f07441f95b2fb2
Priya Power <priyajag72@gmail.com>
2020-07-22 12:43:58 -0600

    Add setup method with object charlotte_bronte with author details hash passed through

--- test/author_test.rb
+++ test/author_test.rb
@@ -5,6 +5,10 @@ require "./lib/author"
 
 class AuthorTest < Minitest::Test
 
+  def setup
+    @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+  end
+
   def test_it_exists
     assert_instance_of Author, @charlotte_bronte
   end
