2c7fead44f9adcc910bf8d3a386160058b9b03b7
Priya Power <priyajag72@gmail.com>
2020-07-22 13:30:52 -0600

    Update test has attributes with assertion for attribute authors

--- test/library_test.rb
+++ test/library_test.rb
@@ -16,6 +16,7 @@ class LibraryTest < Minitest::Test
   def test_it_has_attributes
     assert_equal "Denver Public Library", @dpl.name
     assert_equal [], @dpl.books
+    assert_equal [], @dpl.authors
   end
 
 end
