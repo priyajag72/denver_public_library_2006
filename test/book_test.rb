60493b741b3d27866cb93c1766846dbd5a486b5e
Priya Power <priyajag72@gmail.com>
2020-07-22 12:33:22 -0600

    Add setup with instantiated object @book with param book hash

--- test/book_test.rb
+++ test/book_test.rb
@@ -4,8 +4,12 @@ require "./lib/book"
 
 class BookTest < Minitest::Test
 
+  def setup
+    @book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+  end
+
   def test_it_exists
-    assert_instance_of Book, book
+    assert_instance_of Book, @book
   end
 
   #def test_it_has_attributes
