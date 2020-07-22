eb6e3f6ee8cd3ba0d8312b114cdecf82ffde8a2a
Priya Power <priyajag72@gmail.com>
2020-07-22 13:00:23 -0600

    Update test it can write book for author for assertion to check write also adds to @books empty array

--- test/author_test.rb
+++ test/author_test.rb
@@ -22,6 +22,8 @@ class AuthorTest < Minitest::Test
     jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
     assert_equal Book, jane_eyre.class
     assert_equal "Jane Eyre", jane_eyre.title
+    book2 = @charlotte_bronte.write("Villette", "1853")
+    assert_equal [jane_eyre, book2], @charlotte_bronte.books
   end
 
 end
