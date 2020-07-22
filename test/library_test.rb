6eeaad6758dd5bfb04c469596bef624b5614f745
Priya Power <priyajag72@gmail.com>
2020-07-22 13:27:17 -0600

    Add setup method with object @dpl

--- test/library_test.rb
+++ test/library_test.rb
@@ -5,8 +5,12 @@ require "./lib/library"
 
 class LibraryTest < Minitest::Test
 
+  def setup
+    @dpl = Library.new("Denver Public Library")
+  end
+
   def test_it_exists
-    assert_instance_of Library, dpl
+    assert_instance_of Library, @dpl
   end
 
   #def test_it_has_attributes
