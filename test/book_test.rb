249e89f9ab3b581ea3924f23042bd79dedbd27d7
Priya Power <priyajag72@gmail.com>
2020-07-22 12:32:27 -0600

    Add test it exists to Book Test

--- test/book_test.rb
+++ test/book_test.rb
@@ -4,9 +4,9 @@ require "./lib/book"
 
 class BookTest < Minitest::Test
 
-  #def test_it_exists
-    #assert_instance_of <Class>,
-  #end
+  def test_it_exists
+    assert_instance_of Book, book
+  end
 
   #def test_it_has_attributes
   #end
