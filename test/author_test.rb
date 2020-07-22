5c2836f356db53d1353ba4a7d0941c8cb8a3eebf
Priya Power <priyajag72@gmail.com>
2020-07-22 12:42:38 -0600

    Add author and test file; add minitest syntax to Author Test

---
+++ test/author_test.rb
@@ -0,0 +1,15 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/book"
+require "./lib/author"
+
+class AuthorTest < Minitest::Test
+
+  #def test_it_exists
+    #assert_instance_of <Class>,
+  #end
+
+  #def test_it_has_attributes
+  #end
+
+end
