e154470aa29c7332bab0a0501d054ef41aa260d6
Priya Power <priyajag72@gmail.com>
2020-07-22 13:26:32 -0600

    Add test it exists to Library Test

--- test/library_test.rb
+++ test/library_test.rb
@@ -5,9 +5,9 @@ require "./lib/library"
 
 class LibraryTest < Minitest::Test
 
-  #def test_it_exists
-    #assert_instance_of <Class>,
-  #end
+  def test_it_exists
+    assert_instance_of Library, dpl
+  end
 
   #def test_it_has_attributes
   #end
