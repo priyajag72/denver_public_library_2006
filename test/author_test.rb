1aeb739b59d035da3f598df55e56e9dbeedbce92
Priya Power <priyajag72@gmail.com>
2020-07-22 12:47:26 -0600

    Update test has attributes for assertion @author.books

--- test/author_test.rb
+++ test/author_test.rb
@@ -15,6 +15,7 @@ class AuthorTest < Minitest::Test
 
   def test_it_has_attributes
     assert_equal "Charlotte Bronte", @charlotte_bronte.name
+    assert_equal [], @charlotte_bronte.books
   end
 
 end
