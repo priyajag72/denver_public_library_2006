ca8d5103da02fd099340a511b21852512c7c76fc
Priya Power <priyajag72@gmail.com>
2020-07-22 13:47:12 -0600

    Add test for it can return publication info

--- test/library_test.rb
+++ test/library_test.rb
@@ -21,6 +21,7 @@ class LibraryTest < Minitest::Test
   end
 
   def test_it_can_add_authors_and_books_to_library
+    skip
     charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
     book1 = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
     professor = charlotte_bronte.write("The Professor", "1857")
@@ -31,6 +32,20 @@ class LibraryTest < Minitest::Test
     @dpl.add_author(harper_lee)
     assert_equal [charlotte_bronte, harper_lee], @dpl.authors
     assert_equal [book1, professor, villette, mockingbird], @dpl.books
+  end
+
+  def test_it_can_return_publication_information
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    book1 = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853")
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+    @dpl.add_author(charlotte_bronte)
+    @dpl.add_author(harper_lee)
+    
+    expected = {:start=>"1847", :end=>"1857"}
+    assert_equal expected, @dpl.publication_time_frame_for(charlotte_bronte)
   end
 
 end
