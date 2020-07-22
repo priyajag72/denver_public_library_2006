0f27ffd170401da5dd71fb78a47b9d6629e56192
Priya Power <priyajag72@gmail.com>
2020-07-22 12:38:37 -0600

    Update test has attributions for assertions @book.publication_year

--- test/book_test.rb
+++ test/book_test.rb
@@ -16,6 +16,7 @@ class BookTest < Minitest::Test
     assert_equal "To Kill a Mockingbird",
     @book.title
     assert_equal "Harper Lee", @book.author
+    assert_equal "1960", @book.publication_year
   end
 
 end
